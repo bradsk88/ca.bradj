66604bd09960f8341506d530b9da6d8f7a3db478
bradsk88 <hello@bradj.ca>
2014-07-10 23:40:07 -0600

    Baby's first Node.js website (Initial Commit)

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+ * Module Dependencies
+ */
+var express = require('express'),
+    stylus = require('stylus'),
+    nib = require('nib')
+
+var app = express()
+function compile(str, pth) {
+    return stylus(str)
+        .set('filename',pth)
+        .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+	{ src: __dirname + '/public',
+	compile: compile
+	}
+));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+	res.render('index',{title: "home"});
+
+})
+app.listen(3000)
